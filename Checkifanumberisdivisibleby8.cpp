a51367615ef9b3fe3757c4875a0f457a26cdf4f0
akshay9123 <115215570+akshay9123@users.noreply.github.com>
2024-02-28 09:09:37 +0530

    Create Checkifanumberisdivisibleby8.cpp

---
+++ Checkifanumberisdivisibleby8.cpp
@@ -0,0 +1,20 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int DivisibleByEight(string s){
+        //code here
+        
+        int n = s.length();
+        
+        int l = stoi(s.substr(max(n-3,0)));
+        
+        return l%8==0?1:-1;
+    }
+};
+
